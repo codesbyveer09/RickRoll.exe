3d19cc1ff2953bb2c690917076e42e4a951eb8cb
Veer Mehta <57525741+codesbyveer09@users.noreply.github.com>
2022-03-31 16:28:04 +0530

    Add files via upload

---
+++ Not_a_Joke.py
@@ -0,0 +1,8 @@
+import webbrowser, time
+
+webbrowser.open("https://www.google.com/search?q=what+to+do+when+you+get+rickrolled&rlz=1C1ONGR_enIN956IN956&oq=\
+	What+to+do+after+getting+rick&aqs=chrome.2.69i57j0i13j0i22i30j0i390l3.15421j1j7&sourceid=chrome&ie=UTF-8")
+webbrowser.open("https://youtu.be/dQw4w9WgXcQ?t=10")
+
+print("!!Hahaha Get Rick Rolled!!")
+time.sleep(5)
